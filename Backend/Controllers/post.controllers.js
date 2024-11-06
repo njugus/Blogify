30cda470842c96ef3c1b8a06abfae0968b51acc9
njugus <kayvohnjuguna@gmail.com>
2024-11-06 14:52:34 -0800

    adds an API that enables a user create a post

--- Backend/Controllers/post.controllers.js
+++ Backend/Controllers/post.controllers.js
@@ -4,6 +4,43 @@ import authMiddleware from "../Middleware/auth.js";
 const prisma = new PrismaClient()
 
 //create a Post
+export const createAnewPost = async(req, res) => {
+    const user_id = req.user.id
+    const { title, content, author_id, post_category_id, tags} = req.body
+    try{
+        //check and create the tags if they do not exist
+        const tagIds = await Promise.all(
+            tags.map(async(tagName) => {
+                let tag = await prisma.tag.findUnique({
+                    where : { name : tagName}
+                })
+                if(!tag){
+                    tag = await prisma.tag.create({
+                        data : { name : tagName}
+                    })
+                }
+                return { id : tag.id}
+            })
+        )
+
+        // step 2 create a post 
+        const newPost = await prisma.post.create({
+            data : {
+                title : title,
+                content : content,
+                author_id : author_id,
+                post_category_id : post_category_id,
+                tags : {
+                    connect : tagIds
+                }
+            }
+        })
+
+        res.status(201).json({ success : true, data : newPost})
+    }catch(error){
+        res.status(500).json({ success : false, message : error.message || "Error Creating Post"})
+    }
+}
 
 
 
--- Backend/Controllers/tags.controllers.js
+++ Backend/Controllers/tags.controllers.js
@@ -3,8 +3,3 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 //create tags 
-export const createTags = async(req, res){
-    try{
-        const newTags = 
-    }
-}
